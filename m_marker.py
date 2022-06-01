24bc63be3f2cef81587a8812180613dd842cdd40
Hung.VS <HungICT2407@gmail.com>
2022-06-01 17:10:17 +0700

    [mid-term]: PHAN I - PYTHON CO BAN

--- m_marker.py
+++ m_marker.py
@@ -11,4 +11,12 @@ class Marker:
         
         prob_score=0
         
+        if prob_id==1:
+            prob_score+=0.5 if isinstance(prob_mod.a,float) else 0
+            prob_score+=0.5 if prob_mod.b[1]=='7' else 0
+            prob_score+=0.5 if prob_mod.c=="Hello, World!" else 0
+            prob_score+=0.5 if prob_mod.d(24,7)==24*7 else 0
+            prob_score+=0.5 if prob_mod.e.get_name() == "Hello, World!" else 0
+            prob_score+=0.5 if prob_mod.f["name"]=="Hello, World!" and prob_mod.f["score"]==10 else 0
+        
         prob_rpt.update_score(prob_score)
---
+++ m_submissions/mssv/1.py
@@ -0,0 +1,53 @@
+'''
+PHẦN I: PYTHON CƠ BẢN.
+'''
+
+
+
+
+'''
+a) Khởi tạo biến a có GIÁ TRỊ TÙY Ý thuộc kiểu float.
+'''
+
+
+
+
+'''
+b) Khởi tạo biến b thuộc kiểu List chứa LẦN LƯỢT các phần tử sau: 24, '7', 99.0.
+'''
+
+
+
+
+'''
+c) Khởi tạo biến c thuộc kiểu str có nội dung được NỐI từ c0 và c1.
+'''
+c0="Hello, "
+c1="World!"
+
+
+
+
+'''
+d) Tạo function d nhận 2 tham số a, b và trả về TÍCH của chúng.
+'''
+
+
+
+
+'''
+e)
+e.1. Tạo class E có:
+ - Thuộc tính _name.
+ - Phương thức get_name trả về _name.
+ 
+e.2. Tạo đối tượng e thuộc kiểu E có giá trị của _name là "Hello, World!"
+'''
+
+
+
+
+'''
+f) Tạo đối tượng f thuộc kiểu Dict chứa các cặp (key,value) sau: 
+("name": "Hello, World!"), ("score": 10)
+'''
